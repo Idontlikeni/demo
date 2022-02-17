661d134fde5103fcd3fb71c16a9e7a5f82e3ec58
Student <tima.xxxyyy@mail.ru>
2022-02-17 16:46:26 +0300

    Init

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy =GenerationType.AUTO)
+    public Integer id;
+
+    public String name;
+
+    public String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
